1ed1885276a1794981e96fea2d9369fffcda9dd0
NCEEGEE <74113074+NCEEGEE@users.noreply.github.com>
2021-09-26 22:25:04 -0400

    Delete Utils directory

--- Utils/EditorUtils.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEditor;
-
-namespace PrettyHierarchy
-{
-    public static class EditorUtils
-    {
-        public static bool IsHierarchyFocused { get { return EditorWindow.focusedWindow != null && EditorWindow.focusedWindow.titleContent.text == "Hierarchy"; } }
-    }
-}
